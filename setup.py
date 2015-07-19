228d1075fe6e6d7ba1aefe3c081833ea1637f218
Martin Peeters <martin.peeters@affinitic.be>
2015-07-19 14:37:36 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Installer for the fbk.policy package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='fbk.policy',
+    version='0.1',
+    description="Policy for Kinesiology Belgium website",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3.6",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Python Plone',
+    author='Martin Peeters',
+    author_email='martin.peeters@affinitic.be',
+    url='http://github.com/affinitic/fbk.policy',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['fbk'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'setuptools',
+        'z3c.jbot',
+        'plone.app.dexterity',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
+        ],
+    },
+    entry_points="""
+    """,
+)
---
+++ src/fbk/policy/interfaces.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Module where all interfaces, events and exceptions live."""
+
+from fbk.policy import _
+from zope import schema
+from zope.interface import Interface
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class IFbkPolicyLayer(IDefaultBrowserLayer):
+    """Marker interface that defines a browser layer."""
+
+
+class ITask(Interface):
+
+    title = schema.TextLine(
+        title=_(u"Title"),
+        required=True,
+    )
+
+    description = schema.Text(
+        title=_(u"Description"),
+        required=False,
+    )
+
