b5d0b5c63b6397236ec10e60dd1a02d5b190d1cd
Martin Peeters <martin.peeters@affinitic.be>
2015-07-23 21:02:48 +0200

    Ensure that the base content does not exist to avoid duplicate ones

--- src/fbk/policy/setuphandlers.py
+++ src/fbk/policy/setuphandlers.py
@@ -53,11 +53,12 @@ def create_base_content(portal):
     elements = [u'Kinésiologie', u'La Fédération', u'Congrès',
                 u'Kinésiologues FBK', u'Formations', u'Informations']
     for element in elements:
-        api.content.create(
-            type='Folder',
-            title=element,
-            container=fr_folder,
-        )
+        if element not in fr_folder:
+            api.content.create(
+                type='Folder',
+                title=element,
+                container=fr_folder,
+            )
 
 
 def cleanup_contenttypes():
