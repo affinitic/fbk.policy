1e5fac13fc6822063a842d6cad8d91791817b194
Martin Peeters <martin.peeters@affinitic.be>
2015-08-02 14:04:57 +0200

    Add specific views for kinesiologists

---
+++ src/fbk/policy/browser/kinesiologist.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+fbk.policy
+----------
+
+Created by mpeeters
+:copyright: (c) 2015 by Affinitic SPRL
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from plone.dexterity.browser.view import DefaultView
+
+
+class KinesiologistView(DefaultView):
+    pass
+
+
+class KinesiologistFieldsView(DefaultView):
+    pass
