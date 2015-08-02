70fc55270a3fa2b89648f46fe3535163f0d5ad96
Martin Peeters <martin.peeters@affinitic.be>
2015-08-02 14:07:33 +0200

    Add specific views for formation center content type

---
+++ src/fbk/policy/browser/formationcenter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+fbk.policy
+----------
+
+Created by mpeeters
+:copyright: (c) 2015 by Affinitic SPRL
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from plone.dexterity.browser.view import DefaultView
+
+
+class FormationCenterView(DefaultView):
+    pass
+
+
+class FormationCenterFieldsView(DefaultView):
+    pass
