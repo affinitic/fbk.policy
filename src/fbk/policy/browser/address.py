5291497c15217c3ef2aa73acaf347af9f3379e2b
Martin Peeters <martin.peeters@affinitic.be>
2015-08-08 20:12:00 +0200

    Add the Address content type

---
+++ src/fbk/policy/browser/address.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+fbk.policy
+----------
+
+Created by mpeeters
+:copyright: (c) 2015 by Affinitic SPRL
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from fbk.policy.browser.base import DefaultFieldsView
+
+
+class AddressCustomView(DefaultFieldsView):
+    excluded_fields = (
+        'title',
+    )
---
+++ src/fbk/policy/content/address.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+fbk.policy
+----------
+
+Created by mpeeters
+:copyright: (c) 2015 by Affinitic SPRL
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from five import grok
+from plone.app.multilingual.dx import directives
+from plone.dexterity.content import Item
+from plone.dexterity.schema import DexteritySchemaPolicy
+from zope import schema
+from zope.interface import Interface
+from zope.schema.vocabulary import SimpleVocabulary
+
+from fbk.policy import _
+
+
+class IAddress(Interface):
+
+    title = schema.TextLine(
+        title=_(u'Title'),
+        required=True,
+    )
+
+    street = schema.TextLine(
+        title=_(u'Street'),
+        required=True,
+    )
+
+    additional_address_details = schema.TextLine(
+        title=_(u'Additional address details'),
+        required=False,
+    )
+
+    zip_code = schema.Int(
+        title=_(u'Zip code'),
+        required=True,
+    )
+
+    city = schema.TextLine(
+        title=_(u'City'),
+        required=True,
+    )
+
+    province = schema.TextLine(
+        title=_(u'Province'),
+        required=True,
+    )
+
+    country = schema.Choice(
+        title=_(u'Country'),
+        required=True,
+        vocabulary=SimpleVocabulary.fromValues([
+            _(u'Belgium'),
+            _(u'France'),
+            _(u'Luxembourg'),
+            _(u'Germany'),
+            _(u'Nederland'),
+        ]),
+        default=_(u'Belgium'),
+    )
+
+    directives.languageindependent(
+        'street',
+        'zip_code',
+        'city',
+        'province',
+        'country',
+    )
+
+
+class Address(Item):
+    grok.implements(IAddress)
+
+
+class AddressSchemaPolicy(grok.GlobalUtility, DexteritySchemaPolicy):
+    grok.name('address_schema_policy')
+
+    def bases(self, schema_name, tree):
+        return (IAddress, )
