7211e9cc7828b8c0cd3f35d346aefb5005d8fbfb
Martin Peeters <martin.peeters@affinitic.be>
2015-08-11 17:36:37 +0200

    Hide the formation center link on the formation center view

--- src/fbk/policy/browser/formationevent.py
+++ src/fbk/policy/browser/formationevent.py
@@ -10,8 +10,17 @@ Created by mpeeters
 
 from plone.dexterity.browser.view import DefaultView
 
+from fbk.policy.content.formationcenterfolder import IFormationCenterFolder
+
 
 class FormationEventView(DefaultView):
+
+    def on_formation_center(self):
+        published = self.request.get('PUBLISHED', None)
+        context = getattr(published, '__parent__', None)
+        if IFormationCenterFolder.providedBy(context):
+            return True
+        return False
 
     def update(self):
         self.parent = self.context.aq_parent
