39adeabf7792d8ded0d2a1035ef69028ab414d71
Martin Peeters <martin.peeters@affinitic.be>
2015-09-06 11:35:13 +0200

    Add the listing view for member trainings

---
+++ src/fbk/policy/browser/memberformation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+fbk.policy
+----------
+
+Created by mpeeters
+:copyright: (c) 2015 by Affinitic SPRL
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from fbk.policy.browser.base import DefaultFieldsView
+from fbk.policy.content.kinesiologistfolder import IKinesiologistFolder
+
+
+class MemberFormationView(DefaultFieldsView):
+    excluded_fields = (
+        'title',
+        'description',
+    )
+
+    def on_member(self):
+        published = self.request.get('PUBLISHED', None)
+        context = getattr(published, '__parent__', None)
+        if IKinesiologistFolder.providedBy(context):
+            return True
+        return False
+
+    def update(self):
+        self.parent = self.context.aq_parent
